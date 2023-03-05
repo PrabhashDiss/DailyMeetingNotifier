1205b8255184aaac19ce50cec063f3ccb2ed3006
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2023-03-05 06:00:21 +0530

    Update DailyMeetingReminder.py

--- DailyMeetingReminder.py
+++ DailyMeetingReminder.py
@@ -1,12 +1,5 @@
 import csv
 from datetime import datetime
-from tkinter import *
-import webbrowser
-
-# Create a Tkinter window
-window = Tk()
-window.title("Daily Meeting Reminder")
-window.withdraw()  # Hide the window initially
 
 # Function to display meeting details on the GUI
 def display_meetings():
@@ -25,18 +18,4 @@ def display_meetings():
 
             # If the meeting time matches the current time, display the details and link on the GUI
             if meeting_time == current_time:
-               # Create a Text widget to display the meeting details and link
-                details_text = Text(window, wrap=WORD)
-                details_text.insert(END, meeting_details + "\n")
-                details_text.insert(END, meeting_link, "link")
-                details_text.pack()
-
-                # Create a clickable hyperlink for the link
-                details_text.tag_config("link", foreground="blue", underline=True)
-                details_text.tag_bind("link", "<Button-1>", lambda event: webbrowser.open_new(meeting_link))
-
-                window.deiconify()
-
-# Call the display_meetings function every minute
-window.after(60000, display_meetings)
-window.mainloop()
+                pass
